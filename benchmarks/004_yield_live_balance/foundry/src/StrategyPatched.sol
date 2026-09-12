d2e228cc7d161a07459d1d4f13da5092f874112b
Wonder Olabisi <138243099+Wonderadroit@users.noreply.github.com>
2026-09-12 01:09:28 +0100

    test: add Benchmark 004 Foundry patched source

---
+++ benchmarks/004_yield_live_balance/foundry/src/StrategyPatched.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {MockPoolToken} from "./Pool.sol";
+
+contract StrategyPatched {
+    MockPoolToken public immutable pool;
+    uint256 public poolCached;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(MockPoolToken pool_) {
+        pool = pool_;
+    }
+
+    function seed(address holder, uint256 poolAmount, uint256 shareAmount) external {
+        pool.mint(address(this), poolAmount);
+        poolCached = poolAmount;
+        totalSupply = shareAmount;
+        balanceOf[holder] = shareAmount;
+    }
+
+    function transferShares(address to, uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient shares");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
+
+    function burn(address to) external returns (uint256 poolTokensObtained) {
+        uint256 poolCached_ = poolCached;
+        uint256 totalSupply_ = totalSupply;
+        uint256 burnt = balanceOf[address(this)];
+        require(burnt > 0, "no shares");
+        balanceOf[address(this)] = 0;
+        totalSupply = totalSupply_ - burnt;
+        poolTokensObtained = poolCached_ * burnt / totalSupply_;
+        pool.transfer(to, poolTokensObtained);
+        poolCached = poolCached_ - poolTokensObtained;
+    }
+}
