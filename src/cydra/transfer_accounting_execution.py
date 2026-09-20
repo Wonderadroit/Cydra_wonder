061265c75ccdd49041688c14b90ae07e5c03e762
Wonder Olabisi <138243099+Wonderadroit@users.noreply.github.com>
2026-09-20 10:12:50 +0100

    fix: make transfer accounting renderer dependency-free

--- src/cydra/transfer_accounting_execution.py
+++ src/cydra/transfer_accounting_execution.py
@@ -8,10 +8,9 @@ def generate_transfer_accounting_test(hypothesis,contract_model,target_import,ta
     pragma=contract_model.pragma or "^0.8.20"
     source=f'''// SPDX-License-Identifier: UNLICENSED
 pragma solidity {pragma};
-import {{Test}} from "forge-std/Test.sol";
 import {{ {target_type}, FeeTransferToken }} from "{target_import}";
 
-contract CydraTransferAccountingTest is Test {{
+contract CydraTransferAccountingTest {{
     {target_type} internal vault;
     FeeTransferToken internal token;
     function setUp() public {{
@@ -21,7 +20,7 @@ contract CydraTransferAccountingTest is Test {{
     }}
     function testInboundCreditMatchesActualReceived() public {{
         vault.deposit(100);
-        assertEq(vault.credits(address(this)),vault.assetBalance(),"credit exceeds actual assets");
+        require(vault.credits(address(this)) == vault.assetBalance(), "credit exceeds actual assets");
     }}
 }}
 '''
