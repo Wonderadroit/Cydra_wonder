3edf36b5e73b9b9fa6d336cd6d5ec4e8a239f14d
Wonder Olabisi <138243099+Wonderadroit@users.noreply.github.com>
2026-09-21 12:42:13 +0100

    chore: remove obsolete direct selector import

--- scripts/run_benchmark_029_strict_blind_external_outcome.py
+++ scripts/run_benchmark_029_strict_blind_external_outcome.py
@@ -8,7 +8,6 @@ from pathlib import Path
 
 from cydra.canonical_cycle import run_canonical_differential_cycle
 from cydra.finding_gate import FindingCandidate, evaluate_finding_graph
-from cydra.hypothesis_selection import select_next_hypothesis
 from cydra.hypotheses import Hypothesis as CanonicalHypothesis
 from cydra.impact import ImpactAssessment, ImpactLevel
 from cydra.pipeline import investigate
