2f83215695947748ce065ecae4ed8f5d9677bd40
Wonder Olabisi <138243099+Wonderadroit@users.noreply.github.com>
2026-09-25 00:37:32 +0100

    fix: bind parameter delta with word boundaries

--- src/cydra/sequence_foundry.py
+++ src/cydra/sequence_foundry.py
@@ -84,7 +84,7 @@ def generate_sequence_test_from_experiment(
                 if " + " in expression:
                     amount = expression.rsplit(" + ", 1)[1]
                     for parameter_name, argument in parameter_bindings.items():
-                        amount = re.sub(rf"\\b{re.escape(parameter_name)}\\b", argument, amount)
+                        amount = re.sub(rf"\b{re.escape(parameter_name)}\b", argument, amount)
                     relation_assertions.append(
                         f'        assertEq({getter}, {snapshot_name} + {amount}, '
                         f'"unverified state relation: {expression}");'
@@ -92,7 +92,7 @@ def generate_sequence_test_from_experiment(
                 elif " - " in expression:
                     amount = expression.rsplit(" - ", 1)[1]
                     for parameter_name, argument in parameter_bindings.items():
-                        amount = re.sub(rf"\\b{re.escape(parameter_name)}\\b", argument, amount)
+                        amount = re.sub(rf"\b{re.escape(parameter_name)}\b", argument, amount)
                     relation_assertions.append(
                         f'        assertEq({getter}, {snapshot_name} - {amount}, '
                         f'"unverified state relation: {expression}");'
